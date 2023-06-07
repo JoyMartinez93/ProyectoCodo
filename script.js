b0d76c51ed0c5bd2713bcbec6d21f4db3d51d436
Jordan <salamander939@gmail.com>
2023-06-07 19:06:47 -0300

    cambios

--- script.js
+++
@@ -1,4 +0,0 @@
-
-  document.getElementById('comprar-tickets').addEventListener('click', function() {
-    window.location.href = 'C:\Users\Usuario\todo\OneDrive\Imágenes\ProyectoCodo\tickets.html';
-  });
