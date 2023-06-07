3f7e4a9d5e0bfdfb1d442c065681ce06e65719cb
Jordan <salamander939@gmail.com>
2023-06-07 14:58:11 -0300

    cambios

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+  document.getElementById('comprar-tickets').addEventListener('click', function() {
+    window.location.href = 'https://github.com/JoyMartinez93/ProyectoCodo.git';
+  });
+</script>
